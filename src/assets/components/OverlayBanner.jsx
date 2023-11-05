8a7136b14a6eb4f9923571ca88b7b1675e61b2f0
zanybird <saifur583509@gmail.com>
2023-11-05 16:35:15 +0600

    completed the add job page

---
+++ src/assets/components/OverlayBanner.jsx
@@ -0,0 +1,18 @@
+
+
+const OverlayBanner = () => {
+    return (
+        <div>
+            <div className="relative h-[300px] bg-[url(https://i.ibb.co/wy0d137/card-12.jpg)] bg-cover bg-no-repeat bg-center top-0 w-full">
+                <div className="bg-black absolute top-0 left-0 w-full h-[300px] bg-opacity-75">
+                    <div className="flex flex-col justify-center items-center h-[300px] text-center">
+                        <h1 className="text-white text-5xl font-bold">Add Your Jobs Here</h1>
+                        <p className="text-white text-sm pt-4">Forget the old rules. You can have the best people.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OverlayBanner;
--- src/assets/pages/AddJob/AddJob.jsx
+++ src/assets/pages/AddJob/AddJob.jsx
@@ -1,9 +1,108 @@
+import { useState } from "react";
+import OverlayBanner from "../../components/OverlayBanner";
 
 
 const AddJob = () => {
+
+    const [selectedValue, setSelectedValue] = useState('');
+
+    const handleSelectChange = e => {
+        setSelectedValue(e.target.value);
+    }
+
+    const handleAddJob = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const title = form.title.value;
+        const photo = form.photo.value;
+        const deadline = form.deadline.value;
+        // const category = form.category.value;
+        const minimumPrice = form.minPrice.value;
+        const maximumPrice = form.maxPrice.value;
+        const description = form.description.value;
+        console.log(email, title, photo, minimumPrice, maximumPrice, deadline, selectedValue, description);
+        // const cars = {
+        //     email,
+        //     title,
+        //     photo,
+        //     BrandName,
+        //     BrandPhoto,
+        //     category,
+        //     price,
+        //     Rating,
+        //     Description
+        // };
+    }
+
+
     return (
         <div>
-            Add Job page 
+            <div>
+                <OverlayBanner></OverlayBanner>
+            </div>
+            <div className="w-[85%] mx-auto">
+                <form onSubmit={handleAddJob} className="card-body">
+                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-x-10">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Job Title</span>
+                        </label>
+                        <input type="text" name="title" placeholder="Job Title" className="input input-bordered" required />
+                    </div>
+                     <div className="form-control">
+                        <label className="label">
+                            <span>Job Categories</span>
+                        </label>
+                        <select className="select select-bordered w-full" onChange={handleSelectChange}>
+                            <option>Web Design and Development</option>
+                            <option>Digital Marketing</option>
+                            <option>Graphics Design</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Deadline</span>
+                        </label>
+                        <input type="date" name="deadline" placeholder="date" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Minimum Price</span>
+                        </label>
+                        <input type="number" name="minPrice" placeholder="Minimum Price" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Maximum Price</span>
+                        </label>
+                        <input type="number" name="maxPrice" placeholder="Maximum Price" className="input input-bordered" required />
+                    </div>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Picture</span>
+                        </label>
+                        <input type="text" name="photo" placeholder="Inter Photo URL" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <textarea name="description" placeholder="Inter Description" className="textarea textarea-bordered textarea-lg w-full" required></textarea>
+                    </div>
+                    <div>
+                        <input className="bg-green-600 text-white text-2xl py-3 px-6 rounded-lg w-full cursor-pointer" type="submit" value="Add Job" />
+                    </div>
+                </form>
+            </div>
+
         </div>
     );
 };
